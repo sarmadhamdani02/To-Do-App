b55c9d1d5541a2aa3464126652b8f7eba2903b1a
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2023-09-07 02:00:51 +0500

    same

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 
 const page = () => {
+
+	const [taskName, settaskName] = useState("");
+	const [taskDesc, settaskDesc] = useState("");
+
   return (
-    <div>page</div>
+    <>
+		<navbar className="nav-bar">
+			To Do List
+		</navbar>
+		<div className="main-content">
+            <form >
+                <input type="text" placeholder='Enter Task' />
+                <input type="text" placeholder='Enter Description' />
+            </form>
+			<button className='add-btn'>Add</button>
+        </div>
+	</>
   )
 }
 

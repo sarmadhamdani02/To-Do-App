7f6ee56aefd05fa1104474f2b8f929220dbc8c72
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2023-09-08 00:41:02 +0500

    added more funtionality

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+import { assetPrefix } from '@/next.config';
 import React, { useState } from 'react'
 
 const page = () => {
@@ -7,6 +8,11 @@ const page = () => {
 	const [taskName, settaskName] = useState("");
 	const [taskDesc, settaskDesc] = useState("");
 
+  const addFunction = () => {
+    settaskName("");
+    settaskDesc("");
+  }
+  
   return (
     <>
 		<navbar className="nav-bar">
@@ -14,13 +20,18 @@ const page = () => {
 		</navbar>
 		<div className="main-content">
             <form >
-                <input type="text" placeholder='Enter Task' />
-                <input type="text" placeholder='Enter Description' />
+                <input type="text" placeholder='Enter Task' value= {taskName} onChange={(e) => {
+                   settaskName(e.target.value);
+                }}/>
+                <input type="text" placeholder='Enter Description' value={taskDesc} onChange={() => {
+                  settaskDesc(e.target.value); 
+                }}/>
             </form>
-			<button className='add-btn'>Add</button>
+			<button onClick={addFunction} className='add-btn'>Add</button>
         </div>
 	</>
   )
+  
 }
 
 export default page

17d92be6a6d59a02900295b4d2dea87078817914
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2023-09-09 02:07:07 +0500

    added more funcitonality

--- app/page.js
+++ app/page.js
@@ -1,37 +1,60 @@
 "use client"
 
 import { assetPrefix } from '@/next.config';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const page = () => {
 
-	const [taskName, settaskName] = useState("");
-	const [taskDesc, settaskDesc] = useState("");
+  const [taskName, settaskName] = useState("");
+  const [taskDesc, settaskDesc] = useState("");
+  let [taskPanel, settaskPanel] = useState([]);
+  let renderTask = <h2 className=' text-red-600'>No Task has been added yet.</h2>
 
-  const addFunction = () => {
+  if (taskPanel.length > 0) {
+    
+  
+  renderTask = taskPanel.map((t, i) => { // arguments: target and index repectively
+    return (
+      <div className="task">
+        <h1>{t.taskName}</h1>
+        <h3>{t.taskDesc}</h3>
+      </div>
+    )
+  });
+}
+
+  const addTask = () => {
     settaskName("");
     settaskDesc("");
+
+    settaskPanel([...taskPanel, {taskName, taskDesc}]);
   }
-  
-  return (
-    <>
-		<navbar className="nav-bar">
-			To Do List
-		</navbar>
-		<div className="main-content">
-            <form >
-                <input type="text" placeholder='Enter Task' value= {taskName} onChange={(e) => {
-                   settaskName(e.target.value);
-                }}/>
-                <input type="text" placeholder='Enter Description' value={taskDesc} onChange={() => {
-                  settaskDesc(e.target.value); 
-                }}/>
-            </form>
-			<button onClick={addFunction} className='add-btn'>Add</button>
+
+    return (
+      <>
+        <navbar className="nav-bar">
+          To Do List
+        </navbar>
+        <div className="main-content">
+          <form >
+            <input type="text" placeholder='Enter Task' value={taskName} onChange={(e) => {
+              settaskName(e.target.value);
+            }} />
+            <input type="text" placeholder='Enter Description' value={taskDesc} onChange={(e) => {
+              settaskDesc(e.target.value);
+            }} />
+          </form>
+          <button onClick={addTask} className='add-btn'>Add</button>
         </div>
-	</>
-  )
-  
-}
+        <div className="tasks-div">
+          <ul>
+            <li>{renderTask}</li>
+
+          </ul>
+        </div>
+      </>
+    )
+
+  }
 
 export default page
